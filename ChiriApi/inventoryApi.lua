06f0760cb6d0047545775481a8f68f3f9cc430d9
Chirimorin <Chirimorin@gmail.com>
2021-01-28 13:41:13 +0100

    FindItemsByName/Tag

--- ChiriApi/inventoryApi.lua
+++ ChiriApi/inventoryApi.lua
@@ -8,7 +8,7 @@ local function findItemByName(inventory, itemName)
     expect(1, inventory, "table")
     expect(2, name, "string")
 
-    for i = 1, inventory.size() do
+    for i = 1, inventory.size(), 1 do
         local item = inventory.getItemDetail(i)
         if (item ~= nil and item.name == itemName) then
             return i
@@ -18,13 +18,31 @@ local function findItemByName(inventory, itemName)
     return nil
 end
 
+-- Checks the inventory for items with the given name
+-- Returns a table with the matching items (with the slots as the key)
+local function findItemsByName(inventory, itemName)
+    expect(1, inventory, "table")
+    expect(2, name, "string")
+
+    result = {}
+
+    for i = 1, inventory.size(), 1 do
+        local item = inventory.getItemDetail(i)
+        if (item ~= nil and item.name == itemName) then
+            result[i] = item
+        end
+    end
+
+    return result
+end
+
 -- Checks the inventory for items with the given tag
 -- Returns the slot the item was found in or nil if no items match the given tag
 local function findItemByTag(inventory, itemTag)
     expect(1, inventory, "table")
     expect(2, itemTag, "string")
 
-    for i = 1, inventory.size() do
+    for i = 1, inventory.size(), 1 do
         local item = inventory.getItemDetail(i)
         if (item ~= nil and item.tags ~= nil and item.tags[itemTag] == true) then
             return i
@@ -34,8 +52,27 @@ local function findItemByTag(inventory, itemTag)
     return nil
 end
 
+-- Checks the inventory for items with the given tag
+-- Returns a table with the matching items (with the slots as the key)
+local function findItemsByTag(inventory, itemTag)
+    expect(1, inventory, "table")
+    expect(2, itemTag, "string")
+
+    result = {}
+
+    for i = 1, inventory.size(), 1 do
+        local item = inventory.getItemDetail(i)
+        if (item ~= nil and item.tags ~= nil and item.tags[itemTag] == true) then
+            result[i] = item
+        end
+    end
+
+    return result
+end
 
 return {
     findItemByName = findItemByName,
-    findItemByTag = findItemByTag
+    findItemsByName = findItemsByName,
+    findItemByTag = findItemByTag,
+    findItemsByTag = findItemsByTag
 }
